ddd88ab6f46c31853926310744ce674342f07bac
patternizer <michael.a.taylor@uea.ac.uk>
2021-09-05 00:54:24 +0100

    Fix dash_html_components decrecation error

--- apps/about.py
+++ apps/about.py
@@ -10,8 +10,10 @@
 #------------------------------------------------------------------------------
 
 #import dash
-import dash_core_components as dcc
-import dash_html_components as html
+#import dash_core_components as dcc
+#import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import dash_leaflet as dl
--- index.py
+++ index.py
@@ -10,8 +10,9 @@
 #------------------------------------------------------------------------------
 
 #import dash_core_components as dcc
+#import dash_html_components as html
 from dash import dcc
-import dash_html_components as html
+from dash import html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import dash_leaflet as dl
