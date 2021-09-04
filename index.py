cddb14d9267788b62a66ee682e4a3f848c38d4e1
patternizer <michael.a.taylor@uea.ac.uk>
2021-09-05 00:47:39 +0100

    Fix dash_html_components decrecation error

--- index.py
+++ index.py
@@ -9,7 +9,8 @@
 # patternizer AT gmail DOT com
 #------------------------------------------------------------------------------
 
-import dash_core_components as dcc
+#import dash_core_components as dcc
+from dash import dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
